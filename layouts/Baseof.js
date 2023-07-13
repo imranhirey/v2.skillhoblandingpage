6f7b677ec4b64fd234acae16fc8ad6317da1428d
Imran Nur Hirey <95400512+imranhirey@users.noreply.github.com>
2023-07-13 17:18:26 +0100

    Update Baseof.js

--- layouts/Baseof.js
+++ layouts/Baseof.js
@@ -26,11 +26,11 @@ const Base = ({
 
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-13XTZBBF9X"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  window.dataLayer = window.dataLayer || []
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date())
 
-  gtag('config', 'G-13XTZBBF9X');
+  gtag('config', 'G-13XTZBBF9X')
 </script>
 
         <title>
