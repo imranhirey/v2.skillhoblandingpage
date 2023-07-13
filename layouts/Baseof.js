629c7e28b7e6e68a1685f688213e0fd31e203cfa
Imran Nur Hirey <95400512+imranhirey@users.noreply.github.com>
2023-07-13 17:08:20 +0100

    Update Baseof.js

--- layouts/Baseof.js
+++ layouts/Baseof.js
@@ -22,6 +22,17 @@ const Base = ({
     <>
       <Head>
         {/* title */}
+    
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-13XTZBBF9X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-13XTZBBF9X');
+</script>
+
         <title>
           {plainify(
             meta_title ? meta_title : title ? title : config.site.title
